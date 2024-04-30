78e8b784d8274d8aa93e4a72386fd6f2c496c19e
aitzol sagardui <in1dm3-d@elorrieta.int>
2024-04-30 11:35:07 +0200

    sprint 2 ia bukatuta

--- E4-PROG-GI/src/model/Abestia.java
+++ E4-PROG-GI/src/model/Abestia.java
@@ -55,5 +55,13 @@ public class Abestia extends Audio {
 	 */
 	public void setGustokoena(boolean gustokoena) {
 		this.gustokoena = gustokoena;
+	}
+
+	@Override
+	public String toString() {
+		return "Izena:" + izena + "|| Iraupena: " + iraupena;
 	}	
+	
+	
+	
 }
