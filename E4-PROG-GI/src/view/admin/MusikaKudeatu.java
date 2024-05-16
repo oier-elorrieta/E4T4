96c8d07fe13e5aab0923dbb48e15be1f56ba0013
aitzol sagardui <in1dm3-d@elorrieta.int>
2024-05-16 12:45:03 +0200

    He puesto una cosa mal... muchas J

--- E4-PROG-GI/src/view/admin/MusikaKudeatu.java
+++ E4-PROG-GI/src/view/admin/MusikaKudeatu.java
@@ -141,11 +141,15 @@ public class MusikaKudeatu extends KudeatuPlantilla {
 				JButton img = new JButton();
 				ImageIcon icono = null;
 				try {
-
+System.out.println("antes");
 					icono = new ImageIcon(select.getIrudia().getBytes(1, (int) select.getIrudia().length()));
+					System.out.println("despues");
 
-				} catch (SQLException e1) {
+				} catch (Exception e1) {
+					
 					System.out.println(e1.getMessage());
+					icono = new ImageIcon("src\\DefaultImg\\default.jpg");
+					
 				}
 
 				img.setIcon(icono);
@@ -182,8 +186,9 @@ public class MusikaKudeatu extends KudeatuPlantilla {
 					select.setIrudiaString(insertatzekoIrudia);
 					select.setEzaugarria((String) cboEzaugarria.getSelectedItem());
 					MusikariaDao.aldatuMusikaria(select);
-				} else {
-
+					dispose();
+					JFrameSortu.musikaKudeatuAukeraSortu();
+					
 				}
 
 			}
